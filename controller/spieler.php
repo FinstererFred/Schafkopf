e5455af72143a839dfca50decbd8fb2846587e3a
MO <maximilian.orth@speed4trade.com>
2016-03-11 13:13:27 +0100

    Spieler Model

---
+++ controller/spieler.php
@@ -0,0 +1,40 @@
+<?php
+
+class lottery extends qrClass
+{
+	private $id;
+  private $name;
+	private $kurz;
+	
+
+	function __construct($id = NULL, $name = NULL, $kurz = NULL)
+	{
+		$this->id = $id;
+		$this->name = $name;
+		$this->kurz = $kurz;
+	}
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function getKurz() {
+    return $this->kurz;
+  }
+
+  public function setId($id) {
+    $this->id = $id
+  }
+
+  public function setName($name) {
+    $this->name = $name
+  }
+
+  public function setKurz($kurz) {
+    $this->kurz = $kurz
+  } 
+}
